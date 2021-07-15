d2008fc841d25002be576e380916d8652bef895e
Evil00787 <matititi123@gmail.com>
2021-07-15 17:47:54 +0200

    App strings

---
+++ lib/utils/app_strings.dart
@@ -0,0 +1,8 @@
+class AvatarsAssets {
+  final String playerOne = "1.png";
+  final String playerTwo = "2.png";
+  final String playerThree = "3.png";
+  final String playerFour = "4.png";
+  final String playerFive = "5.png";
+  final String playerSix = "6.png";
+}
