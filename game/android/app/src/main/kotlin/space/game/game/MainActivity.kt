25709432fd43415115345c7f07ecebc72ae1bdf0
Stanisław Góra <stasgora@gmail.com>
2021-07-15 12:41:17 +0200

    Initial commit

---
+++ game/android/app/src/main/kotlin/space/game/game/MainActivity.kt
@@ -0,0 +1,6 @@
+package space.game.game
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ game/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(SpaceGame());
+}
+
+class SpaceGame extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Space game',
+    );
+  }
+}
