fe4d4bd422b90235ec4b3a00789147f321c479e4
Evil00787 <matititi123@gmail.com>
2021-07-15 18:01:55 +0200

    App strings

--- lib/utils/app_strings.dart
+++ lib/utils/app_strings.dart
@@ -1,8 +1,12 @@
 class AvatarsAssets {
-  final String playerOne = "1.png";
-  final String playerTwo = "2.png";
-  final String playerThree = "3.png";
-  final String playerFour = "4.png";
-  final String playerFive = "5.png";
-  final String playerSix = "6.png";
+  static final String playerOne = AppStrings.avatarsPath + "1.png";
+  static final String playerTwo = AppStrings.avatarsPath + "2.png";
+  final String playerThree = AppStrings.avatarsPath + "3.png";
+  static final String playerFour = AppStrings.avatarsPath + "4.png";
+  static final String playerFive = AppStrings.avatarsPath + "5.png";
+  static final String playerSix = AppStrings.avatarsPath + "6.png";
+}
+
+class AppStrings {
+  static final String avatarsPath = "assets/images/avatars/";
 }
